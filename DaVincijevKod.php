70c661084925e026ec8ecde1e8cf9af0322ab3cf
unknown <bornade10@gmail.com>
2024-11-30 20:33:29 +0100

    zadatak1

---
+++ DaVincijevKod.php
@@ -0,0 +1,22 @@
+<?php
+// Name of the document: "DaVincijevKod.php"
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.">
+    <meta name="keywords" content="Da Vinci, kod, kriminalistički triler, Dan Brown">
+    <meta name="author" content="Vaše ime">
+    <title>Da Vincijev kod</title>
+</head>
+<body>
+    <h1>Da Vincijev kod</h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="https://hr.wikipedia.org/Da_Vincijev_kod" target="_blank">Više o Da Vincijevom kodu</a>
+    </p>
+</body>
+</html>
